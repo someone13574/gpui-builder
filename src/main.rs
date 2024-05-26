a87b3fb781fdbea598bbe8353ed25851d1c7a926
Owen Law <owenlaw222@gmail.com>
2024-05-26 13:09:00 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use gpui::*;
+
+struct HelloWorld {
+    text: SharedString,
+}
+
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size_full()
+            .justify_center()
+            .items_center()
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+    }
+}
+
+fn main() {
+    App::new().run(|cx: &mut AppContext| {
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.on_window_should_close(|cx| {
+                cx.remove_window();
+                true
+            });
+
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        });
+    });
+}
