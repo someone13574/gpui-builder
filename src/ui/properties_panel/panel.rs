e4332b04a99f3d79acc82c73ccd52db63f1597b8
Owen Law <owenlaw222@gmail.com>
2024-05-30 13:20:38 -0400

    Properties panel base

---
+++ src/ui/properties_panel/panel.rs
@@ -0,0 +1,22 @@
+use gpui::*;
+
+use crate::appearance::{colors, sizes};
+
+pub struct PropertiesPanel {}
+
+impl PropertiesPanel {
+    pub fn new<V: 'static>(cx: &mut ViewContext<V>) -> View<Self> {
+        cx.new_view(|_| Self {})
+    }
+}
+
+impl Render for PropertiesPanel {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .h_full()
+            .bg(*colors::SIDEBAR_BG)
+            .min_w(*sizes::SIDEBAR_WIDTH)
+            .border_l_1()
+            .border_color(*colors::BORDER)
+    }
+}
